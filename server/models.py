56a28cec66e833e66a0430b82c547be3f88be3f5
Izaaq <odongoizaaq@gmail.com>
2024-08-05 08:42:01 -0400

    adds models and routes

--- server/models.py
+++ server/models.py
@@ -7,7 +7,8 @@ from sqlalchemy.orm import relationship
 class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    second_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(100), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
@@ -56,7 +57,33 @@ class UserRole(db.Model):
      
     def __repr__(self):
           return f'<UserRole {self.id}>'
-     
+      
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    space_id = db.Column(db.Integer, nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    end_time = db.Column(db.DateTime, nullable=False)
+    total_amount = db.Column(db.Float, nullable=False)
+    
+    space = relationship('Space', back_populates='bookings')
+    
+    def __repr__(self):
+      return f'<Booking {self.id}>'
+  
+class Space(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    hourly_price = db.Column(db.Float, nullable=False)
+    daily_price = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    availability = db.Column(db.Boolean, nullable=False, default=True)
+    
+    booking = relationship('Booking', back_populates='spaces')
+    def __repr__(self):
+      return f'<Space {self.id}>'
     
     
 
