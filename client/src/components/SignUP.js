c52d3ce133240204b040ae23d7bcb73e95846feb
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-08-12 20:42:54 +0300

    Adds some styling to the Sign in and Login Pages

--- client/src/components/SignUP.js
+++ client/src/components/SignUP.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import LoginNav from "./LoginNav"
+import LoginNav from "./LoginNav";
 import { useNavigate } from "react-router-dom";
-import "../styling/login.css";
+import "../styling/signin.css";
 
 function SignUpPage() {
     const [formData, setFormData] = useState({
@@ -68,97 +68,100 @@ function SignUpPage() {
     return (
         <div>
             <LoginNav />
-        <div className="signup-container">
-            <div className="signup-card">
-                <h2>Sign Up</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="role">Role:</label>
-                        <select
-                            id="role"
-                            name="role"
-                            value={formData.role}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">Select a role</option>
-                            <option value="user">User</option>
-                            <option value="admin">Admin</option>
-                        </select>
-                    </div>
+            <div className="signup-container">
+                <div className="signup-card">
+                    <h2>Sign Up</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="role">Role:</label>
+                            <select
+                                id="role"
+                                name="role"
+                                value={formData.role}
+                                onChange={handleChange}
+                                required
+                            >
+                                <option value="">Select a role</option>
+                                <option value="user">User</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                        </div>
 
-                    {formData.role && (
-                        <>
-                            <div className="form-group">
-                                <label htmlFor="firstName">First Name:</label>
-                                <input
-                                    type="text"
-                                    id="firstName"
-                                    name="firstName"
-                                    placeholder="Enter your first name"
-                                    value={formData.firstName}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="secondName">Second Name:</label>
-                                <input
-                                    type="text"
-                                    id="secondName"
-                                    name="secondName"
-                                    placeholder="Enter your second name"
-                                    value={formData.secondName}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="email">Email:</label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    placeholder="Enter your email"
-                                    value={formData.email}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password:</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    placeholder="Enter your password"
-                                    value={formData.password}
-                                    onChange={handleChange}
-                                    required
-                                />
-                            </div>
-                            {formData.role === "admin" && (
+                        {formData.role && (
+                            <>
                                 <div className="form-group">
-                                    <label htmlFor="pdfFile">Upload National Identification:</label>
+                                    <label htmlFor="firstName">First Name:</label>
                                     <input
-                                        type="file"
-                                        id="pdfFile"
-                                        name="pdfFile"
-                                        accept=".pdf"
+                                        type="text"
+                                        id="firstName"
+                                        name="firstName"
+                                        placeholder="Enter your first name"
+                                        value={formData.firstName}
                                         onChange={handleChange}
+                                        required
                                     />
                                 </div>
-                            )}
-                        </>
-                    )}
-                    <button type="submit" className="button-submit">Sign Up</button>
-                </form>
-                {adminMessage && <p className="admin-message text-warning fw-bold">{adminMessage}</p>}
-                <div className="login-link">
-                    Already have an account? <a href="/login">Log In</a>
+                                <div className="form-group">
+                                    <label htmlFor="secondName">Second Name:</label>
+                                    <input
+                                        type="text"
+                                        id="secondName"
+                                        name="secondName"
+                                        placeholder="Enter your second name"
+                                        value={formData.secondName}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email:</label>
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        placeholder="Enter your email"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password">Password:</label>
+                                    <input
+                                        type="password"
+                                        id="password"
+                                        name="password"
+                                        placeholder="Enter your password"
+                                        value={formData.password}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                {formData.role === "admin" && (
+                                    <div className="form-group">
+                                        <label htmlFor="pdfFile">Upload National Identification:</label>
+                                        <input
+                                            type="file"
+                                            id="pdfFile"
+                                            name="pdfFile"
+                                            accept=".pdf"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                )}
+                            </>
+                        )}
+                        <button type="submit" className="button-submit">Sign Up</button>
+                    </form>
+                    {adminMessage && <p className="admin-message">{adminMessage}</p>}
+                    <div className="login-link">
+                        Already have an account? <a href="/login">Log In</a>
+                    </div>
+                </div>
+                <div className="signup-image">
+                    <img src="https://images.unsplash.com/photo-1603796846097-bee99e4a601f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2lnbmluZyUyMHBhcGVyc3xlbnwwfHwwfHx8MA%3D%3D" alt="Sign Up" />
                 </div>
             </div>
-        </div>
         </div>
     );
 }
